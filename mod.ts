ea51b134f076360712e7130bbbf91622542ea706
Dan Rosen <drosen@precipart.com>
2020-06-10 19:57:05 -0400

    Initial commit

---
+++ mod.ts
@@ -0,0 +1,42 @@
+import * as log from "https://deno.land/std/log/mod.ts";
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+
+interface Launch {
+  flightNumber: number;
+  mission: string;
+  rocket: string;
+  customers: Array<string>;
+}
+const launches = new Map<number, Launch>();
+
+async function downloadLaunchData() {
+  log.info("downloading launch data...");
+  const response = await fetch("https://api.spacexdata.com/v3/launches", {
+    method: "GET",
+  });
+
+  if (!response.ok) {
+    log.warning("problem downloading launch data");
+    throw new Error("launch data download failed");
+  }
+
+  const launchData = await response.json();
+  for (const launch of launchData) {
+    const payloads = launch["rocket"]["second_stage"]["payloads"];
+    const customers = _.flatMap(payloads, (payload: any) => {
+      return payload["customers"];
+    });
+
+    const flightData = {
+      flightNumber: launch["flight_number"],
+      mission: launch["mission_name"],
+      rocket: launch["rocket"]["rocket_name"],
+      customers: customers,
+    };
+    launches.set(flightData.flightNumber, flightData);
+    log.info(JSON.stringify(flightData));
+  }
+}
+
+await downloadLaunchData();
+log.info(`downloaded data for ${launches.size} SpaceX launches`);
